12eca913299f397332944d6171486460fbb3f96c
ahern88 <ahern88@163.com>
2017-08-13 02:14:34 +0800

    init project

---
+++ src/main/java/com/ailinxi/guava/LoadingCacheTest.java
@@ -0,0 +1,7 @@
+package com.ailinxi.guava;
+
+/**
+ * Created by messi01.ai on 2017/8/13.
+ */
+public class LoadingCacheTest {
+}
---
+++ src/main/java/com/ailinxi/zookeeper/ZkClientTest.java
@@ -0,0 +1,7 @@
+package com.ailinxi.zookeeper;
+
+/**
+ * Created by messi01.ai on 2017/8/13.
+ */
+public class ZkClientTest {
+}
